29f87798e047aa0ef2521a273dea4d8ef6b1fbcd
Prateek Srivastava <f2prateek@gmail.com>
2016-05-09 01:37:44 -0600

    remove factory function

--- builtin_parser.go
+++ builtin_parser.go
@@ -29,17 +29,16 @@ func (p *kindParser) Parse(t reflect.Type, v string) (interface{}, error) {
 	return p.f(v)
 }
 
-func newKindParser(k reflect.Kind, f func(v string) (interface{}, error)) Parser {
-	return &kindParser{
-		k: k,
-		f: f,
-	}
+var boolParser = &kindParser{
+	k: reflect.Bool,
+	f: func(v string) (interface{}, error) {
+		return strconv.ParseBool(v)
+	},
 }
 
-var boolParser = newKindParser(reflect.Bool, func(v string) (interface{}, error) {
-	return strconv.ParseBool(v)
-})
-
-var stringParser = newKindParser(reflect.String, func(v string) (interface{}, error) {
-	return v, nil
-})
+var stringParser = &kindParser{
+	k: reflect.String,
+	f: func(v string) (interface{}, error) {
+		return v, nil
+	},
+}
