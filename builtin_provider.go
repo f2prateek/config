83588709fed7a8fa90507ab2709be3da79ec0f0f
Prateek Srivastava <f2prateek@gmail.com>
2016-05-09 02:26:31 -0600

    os.args provider

--- builtin_provider.go
+++ builtin_provider.go
@@ -6,6 +6,19 @@ var EnvProvider = providerFunc(func(s string) string {
 	return os.Getenv(s)
 })
 
+var OSArgsProvider = providerFunc(func(s string) string {
+	for i, arg := range os.Args {
+		if arg != "--"+s {
+			continue
+		}
+		if i == len(os.Args)-1 {
+			return ""
+		}
+		return os.Args[i+1]
+	}
+	return ""
+})
+
 type providerFunc func(string) string
 
 func (f providerFunc) Provide(s string) string {
--- builtin_provider_test.go
+++ builtin_provider_test.go
@@ -8,9 +8,32 @@ import (
 )
 
 func TestEnvProvider(t *testing.T) {
-	os.Setenv("name", "prateek")
-	defer os.Setenv("name", "")
+	defer setEnv("name", "prateek")()
 
 	assert.Equal(t, "prateek", EnvProvider.Provide("name"))
 	assert.Equal(t, "", EnvProvider.Provide("foo"))
+}
+
+func TestOSArgsProvider(t *testing.T) {
+	defer setArgs("test", "--foo", "bar", "--qaz")()
+
+	assert.Equal(t, "bar", OSArgsProvider.Provide("foo"))
+	assert.Equal(t, "", OSArgsProvider.Provide("bar"))
+	assert.Equal(t, "", OSArgsProvider.Provide("qaz"))
+}
+
+func setEnv(k, v string) func() {
+	original := os.Getenv(k)
+	os.Setenv(k, v)
+	return func() {
+		os.Setenv(k, original)
+	}
+}
+
+func setArgs(args ...string) func() {
+	original := os.Args
+	os.Args = args
+	return func() {
+		os.Args = original
+	}
 }
